ec242a0decad67ab3bcb7ccaa8d88176d878d41b
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-09-29 00:14:22 +0000

    Pair Programming - Word Search Activity Completed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
     }
-}
+    let verticalJoin = [];
+    for (let index = 0; index < letters[0].length; index++) {
+        let verticalWord = '';
+        for (let horizontalArray of letters) {
+            verticalWord += horizontalArray[index]
+        }
+        verticalJoin.push(verticalWord);
+    } 
+    for (let v of verticalJoin) {
+        if (v.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+};
 
 module.exports = wordSearch
